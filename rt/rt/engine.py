8979f1f812eab33f809eef80ffe661b3b2fe0704
Hampus Fröjdholm <hampus.frojdholm@gmail.com>
2023-11-13 20:35:46 +0100

    Add a simple engine and server

---
+++ rt/rt/engine.py
@@ -0,0 +1,189 @@
+import sys
+import fileinput
+import random
+from abc import ABC, abstractmethod
+from enum import Enum, auto
+
+from rt.state import GameState, Move, Player
+
+
+class Agent(ABC):
+    @abstractmethod
+    def __init__(self, player):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_state(self, state):
+        raise NotImplementedError
+
+    @abstractmethod
+    def search(self, btime, wtime, binc, winc):
+        raise NotImplementedError
+
+
+class RandomAgent(Agent):
+    def __init__(self, player):
+        self.player = player
+        self.state = None
+
+    def set_state(self, state):
+        self.state = state
+
+    def search(self, btime, wtime, binc, winc):
+        moves = self.state.possible_moves(self.player)
+        move = random.choice(moves)
+        return str(move)
+
+
+class EngineError(Exception):
+    pass
+
+
+def expect(expected, actual):
+    if expected != actual:
+        raise EngineError(f"expected: {expected}, got: {actual}")
+
+
+class Engine:
+    """An `Engine` is a state machine that drives the communication with the UI.
+
+    It implements the engine part of the RT v1 protocol and can be used
+    to more easily pass data to and from an `Agent`. The engine will parse
+    commands and translate them to calls to the `Agent` interface.
+
+    If a command is unexpected or wrong an `EngineError` will be raised.
+    """
+
+    class State(Enum):
+        Uninitialized = auto()
+        """The engine is unitialized and awaiting the "reversi_v1" command."""
+        AwaitingNewGame = auto()
+        """The engine has been initialized and sent its id commands, but no game has been started."""
+        NewGameCompleted = auto()
+        """A game has been created and the engine is awaiting "isready"."""
+        AwaitingPosition = auto()
+        """The engine is ready to receive positions."""
+        PositionParsed = auto()
+        """The position has been parsed and the engine is awaiting "isready"."""
+        AwaitingGo = auto()
+        """The engine is ready to search and is awaiting the "go" command."""
+
+    def __init__(self, agent_factory):
+        """Create a new Engine.
+
+        Args:
+            agent_factory: A callable that when called with "b" or "w" will
+                           create a new agent ready to play as black or white
+                           respectively.
+        """
+        self.agent_factory = agent_factory
+        self.state = Engine.State.Uninitialized
+        self.name = "TESTENGINE 1.0"
+        self.author = "<author>"
+        self.game_state = None
+        self.agent = None
+
+    def parse(self, msg):
+        """Parse a message from the UI.
+
+        This function will respond to the message by sending text on `stdout`.
+        Multiple messages might we written and `stdout` will not be flushed.
+
+        Args:
+            msg: The message from the UI.
+
+        Raises:
+            EngineError: If the message was unexpected or wrong.
+        """
+        msg = msg.strip()
+        command, *args = msg.split()
+
+        # The newgame command is allowed any time after the engine has
+        # been initialized. In that case we intercept it here and reset
+        # the engine state.
+        if command == "newgame" and self.state != Engine.State.Uninitialized:
+            self.state = Engine.State.AwaitingNewGame
+            # TODO: Resetting the state here is not really necessary since it
+            # should be overwritten by the next UI commands anyway
+            self.agent = None
+            self.game_state = None
+
+        match self.state:
+            case Engine.State.Uninitialized:
+                expect("reversi_v1", command)
+                self.send_id()
+                self.state = Engine.State.AwaitingNewGame
+            case Engine.State.AwaitingNewGame:
+                expect("newgame", command)
+                self.setup_agent(args)
+                self.state = Engine.State.NewGameCompleted
+            case Engine.State.NewGameCompleted:
+                expect("isready", command)
+                self.send_ready_ok()
+                self.state = Engine.State.AwaitingPosition
+            case Engine.State.AwaitingPosition:
+                expect("position", command)
+                self.parse_position(args)
+                self.state = Engine.State.PositionParsed
+            case Engine.State.PositionParsed:
+                expect("isready", command)
+                self.send_ready_ok()
+                self.state = Engine.State.AwaitingGo
+            case Engine.State.AwaitingGo:
+                expect("go", command)
+                self.send_best_move(args)
+                self.state = Engine.State.AwaitingPosition
+
+    def send_id(self):
+        Engine.respond(f"id name {self.name}")
+        Engine.respond(f"id author {self.author}")
+        Engine.respond("reversi_v1_ok")
+
+    def setup_agent(self, args):
+        if len(args) != 1:
+            raise EngineError(f"invalid newgame args: {args}")
+        p = args[0].lower()
+        assert p == "b" or p == "w"
+        player = Player.Black if p == "b" else Player.White
+        self.agent = self.agent_factory(player)
+
+    def send_best_move(self, args):
+        if len(args) != 4:
+            raise EngineError(f"invalid go args: {args}")
+        kwargs = {}
+        for a in args:
+            key, value = a.split("=")
+            kwargs[key] = int(value)
+        # TODO: Make this robust in case an argument is wrong
+        move = self.agent.search(**kwargs)
+        Engine.respond(f"bestmove {move}")
+
+    def send_ready_ok(self):
+        Engine.respond("readyok")
+
+    def parse_position(self, moves):
+        expect("startpos", moves[0])
+        state = GameState.start_position()
+        for m in moves[1:]:
+            # TODO: Raise an error if the move is invalid
+            move = Move.from_str(m, state)
+            state = state.make_move(move)
+
+        self.agent.set_state(state)
+
+    @staticmethod
+    def respond(msg):
+        # Strip to ensure only a single newline is written
+        msg = msg.strip()
+        sys.stdout.write(msg + "\n")
+
+
+def run():
+    engine = Engine(RandomAgent)
+    for msg in fileinput.input(encoding="utf-8"):
+        try:
+            engine.parse(msg)
+            # Flush to ensure the message is sent to the UI.
+            sys.stdout.flush()
+        except EngineError as err:
+            print(err, file=sys.stderr)
---
+++ rt/rt/server.py
@@ -0,0 +1,173 @@
+import argparse
+import subprocess
+import shlex
+import shutil
+
+from rt.state import Player, GameState
+
+
+def popen(cmd):
+    return subprocess.Popen(
+        shlex.split(cmd),
+        bufsize=1,  # line buffered
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+
+
+class PlayerError(Exception):
+    pass
+
+
+class PlayerProcess:
+    def __init__(self, cmd):
+        self.cmd = shutil.which(cmd)
+        self.name = None
+        self.author = None
+        self.process = None
+
+    def __enter__(self):
+        self.process = popen(self.cmd)
+        return self
+
+    def __exit__(self, *args):
+        self.process.stdin.close()
+        self.process.stdout.close()
+        self.process.terminate()
+        return False
+
+    def __str__(self):
+        return f"{self.name} by {self.author}"
+
+    def _write(self, msg):
+        msg = msg.strip()
+        self.process.stdin.write(msg + "\n")
+
+    def _read(self):
+        msg = self.process.stdout.readline()
+        return msg.strip()
+
+    def expect_enter(self):
+        if self.process is None:
+            raise PlayerError
+
+    def start(self):
+        self.expect_enter()
+        self._write("reversi_v1")
+        while True:
+            line = self._read()
+            if line == "reversi_v1_ok":
+                break
+            id_, command, *value = line.split()
+            assert id_ == "id"
+            if command == "name":
+                self.name = " ".join(value)
+            elif command == "author":
+                self.author = " ".join(value)
+            else:
+                raise PlayerError
+
+    def newgame(self, player):
+        self.expect_enter()
+        self._write(f"newgame {player.value}")
+
+    def isready(self):
+        self.expect_enter()
+        self._write("isready")
+        msg = self._read()
+        if msg != "readyok":
+            raise PlayerError
+
+    def send_position(self, position):
+        self.expect_enter()
+        self._write(f"position {position}")
+
+    def send_go(self, btime, wtime, binc, winc):
+        self.expect_enter()
+        self._write(f"go btime={btime} wtime={wtime} binc={binc} winc={winc}")
+        response = self._read()
+        command, move = response.split()
+        if command != "bestmove":
+            raise PlayerError
+        return move
+
+
+def get_move(player_proc, player, moves, state):
+    # TODO: Implement timing
+    btime = 10000
+    wtime = 10000
+    binc = 10000
+    winc = 10000
+
+    player_proc.send_position(" ".join(moves))
+    player_proc.isready()
+    bestmove = player_proc.send_go(btime=btime, wtime=wtime, binc=binc, winc=winc)
+    for move in state.possible_moves(player):
+        if str(move) == bestmove:
+            return move
+
+    # Invalid move is being played
+    return None
+
+
+def play_game(pb, pw):
+    pb.newgame(Player.Black)
+    pw.newgame(Player.White)
+    pb.isready()
+    pw.isready()
+
+    moves = ["startpos"]
+    state = GameState.start_position()
+    while (next_player := state.next_player()) is not None:
+        if next_player == Player.Black:
+            bestmove = get_move(pb, next_player, moves, state)
+        elif next_player == Player.White:
+            bestmove = get_move(pw, next_player, moves, state)
+
+        # The player made an illegal move so the opponent wins
+        if bestmove is None:
+            return next_player.opponent()
+
+        moves.append(str(bestmove))
+        state = state.make_move(bestmove)
+    # When the game is over the winner of the last state wins the game
+    return state.winner()
+
+
+def run_server(player1, player2):
+    iterations = 100
+    wins_p1 = 0
+    wins_p2 = 0
+    draws = 0
+    with PlayerProcess(player1) as p1, PlayerProcess(player2) as p2:
+        p1.start()
+        p2.start()
+        print(p1, "vs", p2)
+        for _ in range(iterations // 2):
+            winner = play_game(p1, p2)
+            if winner == Player.Black:
+                wins_p1 += 1
+            elif winner == Player.White:
+                wins_p2 += 1
+            else:
+                draws += 1
+        for _ in range(iterations // 2):
+            winner = play_game(p2, p1)
+            if winner == Player.Black:
+                wins_p2 += 1
+            elif winner == Player.White:
+                wins_p1 += 1
+            else:
+                draws += 1
+    print("P1:", wins_p1, "P2:", wins_p2, "Draw:", draws)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("player1")
+    parser.add_argument("player2")
+
+    args = parser.parse_args()
+    run_server(args.player1, args.player2)
